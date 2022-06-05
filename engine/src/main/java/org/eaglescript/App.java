d72c9b07fb2329b1f811514b2336cb050a6e415c
Gang Chen <cgdlut@gmail.com>
2022-06-05 14:51:31 +0800

    Added skeleton code.

---
+++ engine/src/main/java/org/eaglescript/App.java
@@ -0,0 +1,13 @@
+package org.eaglescript;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
