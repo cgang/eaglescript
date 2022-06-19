505f69b59736772cfe7d823031bf19c020b5cc3c
Gang Chen <cgdlut@gmail.com>
2022-06-19 21:38:38 +0800

    Added more basic instructions.

--- engine/src/main/java/org/eaglescript/vm/Instructions.java
+++ engine/src/main/java/org/eaglescript/vm/Instructions.java
@@ -8,6 +8,11 @@ import java.lang.reflect.Modifier;
  */
 class Instructions {
     /**
+     * Do nothing.
+     */
+    static final int NOP = 0;
+
+    /**
      * Load reference from variable.
      * Format:
      * <pre>LOAD index</pre>
@@ -41,6 +46,129 @@ class Instructions {
     static final int IF_TRUE = 0x21;
 
     static final int IF_FALSE = 0x22;
+
+    /**
+     * Un-determined plus operation, concatenation or arithmetic addition.
+     * Format:
+     * <pre>PLUS</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value1, value2 ->
+     * ..., result
+     * </pre>
+     */
+    static final int PLUS = 0x2F;
+
+    /**
+     * Arithmetic addition.
+     * Format:
+     * <pre>ADD</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value1, value2 ->
+     * ..., result
+     * </pre>
+     */
+    static final int ADD = 0x30;
+    /**
+     * Arithmetic subtraction.
+     * Format:
+     * <pre>SUB</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value1, value2 ->
+     * ..., result
+     * </pre>
+     */
+    static final int SUB = 0x31;
+    /**
+     * Arithmetic multiplication.
+     * Format:
+     * <pre>MUL</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value1, value2 ->
+     * ..., result
+     * </pre>
+     */
+    static final int MUL = 0x32;
+    /**
+     * Arithmetic division.
+     * Format:
+     * <pre>DIV</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value1, value2 ->
+     * ..., result
+     * </pre>
+     */
+    static final int DIV = 0x33;
+    /**
+     * Negate a number.
+     * Format:
+     * <pre>NEG</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value ->
+     * ..., result
+     * </pre>
+     */
+    static final int NEG = 0x34;
+    /**
+     * Duplicate the top operand stack value.
+     * Format:
+     * <pre>DUP</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value ->
+     * ..., value, value
+     * </pre>
+     */
+    static final int DUP = 0x35;
+    /**
+     * Duplicate the top operand stack value and insert two values down.
+     * Format:
+     * <pre>DUP_X1</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value2, value1 ->
+     * ..., value1, value2, value1
+     * </pre>
+     */
+    static final int DUP_X1 = 0x36;
+    /**
+     * Duplicate the top operand stack value and insert three values down.
+     * Format:
+     * <pre>DUP_X2</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value3, value2, value1 ->
+     * ..., value1, value3, value2, value1
+     * </pre>
+     */
+    static final int DUP_X2 = 0x37;
+    /**
+     * Pop the top operand stack value.
+     * Format:
+     * <pre>POP</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value ->
+     * ...,
+     * </pre>
+     */
+    static final int POP = 0x38;
+    /**
+     * Swap the top two operand stack values.
+     * Format:
+     * <pre>SWAP</pre>
+     * Operand stack:
+     * <pre>
+     * ..., value2, value1 ->
+     * ..., value1, value2
+     * </pre>
+     */
+    static final int SWAP = 0x39;
 
     static final int SIMPLE = 0xA0;
 
--- engine/src/main/java/org/eaglescript/vm/Operations.java
+++ engine/src/main/java/org/eaglescript/vm/Operations.java
@@ -1,5 +1,32 @@
 package org.eaglescript.vm;
 
 class Operations {
+    static Object plus(Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) {
+            return (Double) left + (Double) right;
+        } else {
+            // TODO be comply with ES6 spec.
+            return String.valueOf(left) + right;
+        }
+    }
 
+    static Object add(Object left, Object right) {
+        return (Double) left + (Double) right;
+    }
+
+    static Object sub(Object left, Object right) {
+        return (Double) left - (Double) right;
+    }
+
+    static Object mul(Object left, Object right) {
+        return (Double) left * (Double) right;
+    }
+
+    static Object div(Object left, Object right) {
+        return (Double) left / (Double) right;
+    }
+
+    static Object negate(Object value) {
+        return - (Double) value;
+    }
 }
