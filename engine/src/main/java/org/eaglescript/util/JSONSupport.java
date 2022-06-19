605794c0af951fe049972bd640efa9598c8982e0
Gang Chen <cgdlut@gmail.com>
2022-06-19 11:13:49 +0800

    Added more built-in objects.

--- engine/src/main/java/org/eaglescript/util/JSONSupport.java
+++ engine/src/main/java/org/eaglescript/util/JSONSupport.java
@@ -2,6 +2,10 @@ package org.eaglescript.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * The {@link JSONSupport} provides basic JSON related support for script.
+ * It's intended to be exposed as 'JSON' to script.
+ */
 public class JSONSupport {
     private final ObjectMapper mapper;
 
